8c8e2bc3baeb4003575a7ecd1942aaf888cca240
vmruiz88 <79539458+vmruiz88@users.noreply.github.com>
2021-02-23 12:12:12 -0700

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ Victor Ruiz
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum; 
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +15,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        return num+previousNumber;
     }
     
     /*****
@@ -41,9 +42,10 @@ public class GitDemo {
      *****/
     public double reverseNumber(double num){
         double newNumber = 1;
-        
+        reverseNum = newNumber; 
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
+            
         }
         
         return newNumber;
